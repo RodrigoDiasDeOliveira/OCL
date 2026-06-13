626b89fbd56fc006b2e365543b5f10b83ec9d971
RodrigoDiasDeOliveira <rodrigodigau02@gmail.com>
2026-06-13 10:01:12 +0000

    source code uograde websockets

---
+++ TLP-rfid/backend/src/main/java/com/triminds/websocket/RfidWebSocketHandler.java
@@ -0,0 +1,17 @@
+package com.triminds.tlp.websocket;
+
+import com.triminds.tlp.rfid.dto.RfidEventDTO;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class RfidWebSocketHandler {
+
+    @MessageMapping("/rfid-event")
+    @SendTo("/topic/rfid-events")
+    public RfidEventDTO handleRfidEvent(RfidEventDTO event) {
+        System.out.println("WebSocket - Evento RFID recebido: " + event.getTagId());
+        return event;
+    }
+}
